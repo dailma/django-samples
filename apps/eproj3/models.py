71ffb4f4ed5a51610e5e0751c7ec07aa9e135f7d
Dail Magee Jr <dailma@msn.com>
2016-07-27 17:37:32 -0700

    State at EoE

--- apps/eproj3/models.py
+++ apps/eproj3/models.py
@@ -4,32 +4,98 @@ import datetime
 import bcrypt
 
 class UserManager(models.Manager):
-	def create_user(self):  # TODO: add params
-		error_msgs = []
-		# TODO: validations
-		error_msgs.append("Placeholder error message")
-		if error_msgs:
-			return (False, error_msgs)
-		# TODO: call User.objects.create() and
-		# return (True, user_obj)
-		pass  # TODO: remove
+	def get_user(self, username):
+		try:
+			user_obj = User.objects.get(username=username)
+			return (True, user_obj)
+		except:
+			return (False, "Username not found")
 
-	def login(self):  # TODO: add params
+	def create(self, name, username, password, pw_conf):
 		error_msgs = []
-		# TODO: validations
-		error_msgs.append("Placeholder error message")
+		if len(name) < 3:
+			error_msgs.append("Name must contain at least 3 characters")
+		if len(username) < 3:
+			error_msgs.append("Username must contain at least 3 characters")
+		else:
+			result_success, result_obj = self.get_user(username)
+			if result_success:
+				error_msgs.append("Username already registered")
+		if len(password) < 8:
+			error_msgs.append("Password must contain at least 8 characters")
+		elif password != pw_conf:
+			error_msgs.append("Passwords do not match. Try again")
 		if error_msgs:
 			return (False, error_msgs)
-		# TODO: call User.objects.login() and
-		# return (True, user_obj)
-		pass  # TODO: remove
+		else:
+			pw_hash = bcrypt.hashpw( password.encode(), bcrypt.gensalt() )
+			user_obj = User.objects.create(
+				name=name,
+				username=username,
+				pw_hash=pw_hash )
+			return (True, user_obj)
+
+	def login(self, username, password):
+		error_msgs = []
+		result_success, result_obj = self.get_user(username)
+		if not result_success:
+			error_msgs.append(result_obj)
+		elif not bcrypt.hashpw( password.encode(),
+			result_obj.pw_hash.encode() ) == result_obj.pw_hash:
+			error_msgs.append("Incorrect password")
+		if error_msgs:
+			return (False, error_msgs)
+		else:
+			return (True, result_obj)
 # end UserManager
+
+class WishManager(models.Manager):
+	def get_all(self):
+		wish_obj = Wish.objects.all()
+		return wish_obj
+
+	def get_one(self, id):
+		wish_obj = Wish.objects.get(id=id)
+		return wish_obj
+
+	def create(self, product, user_id):
+		error_msgs = []
+		if len(product) < 3:
+			error_msgs.append("Name must contain at least 3 characters")
+		if error_msgs:
+			return (False, error_msgs)
+		user_obj = User.objects.get(id=user_id)
+		wish_obj = Wish.objects.create(product=product, creator=user_obj)
+		return (True, wish_obj)
+
+	def destroy(self, id):
+		Wish.objects.get(id=id).delete()
+		return True
+
+	def link(self, wish_id, user_id):
+		user_obj = User.objects.get(id=user_id)
+		return True
+
+	def unlink(self, wish_id, user_id):
+		user_obj = User.objects.get(id=user_id)
+		return True
+# end WishManager
 
 class User(models.Model):
 	name = models.CharField(max_length=255)
 	username = models.CharField(max_length=100)
 	pw_hash = models.CharField(max_length=255)
+	# date_hired = models.DateTimeField(auto_now_add = True)  # on wireframe but never used
 	created_at = models.DateTimeField(auto_now_add = True)
 	updated_at = models.DateTimeField(auto_now = True)
 	userManager = UserManager()
+	objects = models.Manager()
+
+class Wish(models.Model):
+	product = models.CharField(max_length=255)
+	creator = models.ForeignKey(User, related_name="creator")
+	metoo = models.ManyToManyField(User, related_name="metoo")
+	created_at = models.DateTimeField(auto_now_add = True)
+	updated_at = models.DateTimeField(auto_now = True)
+	wishManager = WishManager()
 	objects = models.Manager()
--- apps/eproj3/views.py
+++ apps/eproj3/views.py
@@ -1,36 +1,84 @@
 from django.shortcuts import redirect, render
 from django.core.urlresolvers import reverse
 from django.contrib import messages
-from .models import User  #  TODO: and any other models
+from .models import User, Wish
 
 # rendering functions
 def index(request):
-	context = {"items": ["Item 1", "Item 2"] }
+	if not "user_id" in request.session:
+		return redirect(reverse("eproj3_signin"))
+	context = { "products": Wish.wishManager.get_all() }
 	return render(request, "eproj3/index.html", context)
 
 def signin(request):
+	if "user_id" in request.session:
+		return redirect(reverse("eproj3_index"))
 	return render(request, "eproj3/signin.html")
+
+def showitem(request, id):
+	context = { "product": Wish.wishManager.get_one(id), "users": ["Dail", "Tim"] }
+	return render(request, "eproj3/showitem.html", context)
+
+def additem(request):
+	return render(request, "eproj3/additem.html")
 
 # redirecting functions
 def register(request):
 	if request.method == "POST":
-		result_success, result_obj = User.userManager.create_user()  #  TODO: add arguments
+		result_success, result_obj = User.userManager.create(			request.POST["reg_name"],
+			request.POST["reg_username"], 
+			request.POST["reg_password"],
+			request.POST["reg_password_confirm"] )
 		if not result_success:
 			for msg in result_obj:
 				messages.add_message(request, messages.ERROR, msg, extra_tags="reg")
 			return redirect(reverse("eproj3_signin"))
-		pass  #  TODO: stuff like request.session and redirect
+		request.session["user_id"] = result_obj.id
+		request.session["name"] = result_obj.name
+		return redirect(reverse("eproj3_index"))
 
 def login(request):
 	if request.method == "POST":
-		result_success, result_obj = User.userManager.login()  #  TODO: add arguments
+		result_success, result_obj = User.userManager.login(
+			request.POST["log_username"],
+			request.POST["log_password"] )
 		if not result_success:
 			for msg in result_obj:
 				messages.add_message(request, messages.ERROR, msg, extra_tags="log")
 			return redirect(reverse("eproj3_signin"))
-		pass  #  TODO: stuff like request.session and redirect
+		request.session["user_id"] = result_obj.id
+		request.session["name"] = result_obj.name
+		return redirect(reverse("eproj3_index"))
 
 def logout(request):
-	#  TODO: zap request.session keys
-	#  TODO: add success message? (see views.py in journeys_plus)
+	if "user_id" in request.session:
+		del request.session["user_id"]
+	if "name" in request.session:
+		del request.session["name"]
+	messages.add_message(request, messages.SUCCESS,
+						 "You've successfully logged out.",
+						 extra_tags="log")
 	return redirect(reverse("eproj3_signin"))
+
+def linkitem(request, id):
+	Wish.wishManager.link(id, request.session["user_id"])
+	return redirect(reverse("eproj3_index"))
+
+def unlinkitem(request, id):
+	Wish.wishManager.unlink(id, request.session["user_id"])
+	return redirect(reverse("eproj3_index"))
+
+def createitem(request):
+	if request.method == "POST":
+		result_success, result_obj = Wish.wishManager.create( 
+			request.POST["inp_product"],
+			request.session["user_id"] )
+		if not result_success:
+			for msg in result_obj:
+				messages.add_message(request, messages.ERROR, msg, extra_tags="item")
+			return redirect(reverse("eproj3_additem"))
+		return redirect(reverse("eproj3_index"))
+
+def destroyitem(request, id):
+	Wish.wishManager.destroy(id)
+	return redirect(reverse("eproj3_index"))
